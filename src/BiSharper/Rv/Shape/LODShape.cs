3a476eed6836c3286210173bded94520d893b42f
ryann <flipperplz@protonmail.com>
2023-11-13 08:22:47 -0800

    feat(rv/shape): Add Some More Logic To Shape Parsing

--- src/BiSharper/Rv/Shape/LODShape.cs
+++ src/BiSharper/Rv/Shape/LODShape.cs
@@ -6,9 +6,9 @@ namespace BiSharper.Rv.Shape;
 
 public class LODShape
 {
-
     private ShapeRemarks _remarks = 0;
     private float _mass = 0;
+    private List<float> _massArray = new List<float>();
     private float _inverseMass;
     private Vector3 _centerOfMass;
     private BTripointMatrix3 _inverseInertia;
@@ -26,8 +26,36 @@ public class LODShape
         _inverseInertia = new BTripointMatrix3();
         _inverseInertia = new BTripointMatrix3();
         _centerOfMass = new Vector3();
-        
 
+        var signature = reader.ReadBytes(4);
+        if (signature == "MLOD"u8)
+        {
+            tagged = true;
+            version = reader.ReadInt32();
+            lodCount = reader.ReadInt32();
+        } else if (signature == "NLOD"u8)
+        {
+            tagged = true;
+            lodCount = reader.ReadInt32();
+        } else if (signature == "ODOL"u8)
+        {
+            //TODO
+        }
+
+        int versionMajor = version >> 8, versionMinor = version & 0xff;
+        if (versionMajor > 1)
+        {
+            throw new NotSupportedException($"Unsupported Shape Version {versionMajor}.{versionMinor}");
+        }
+
+        bool treeCrownNeeded = false, canBlend = false;
+        for (var i = 0; i < lodCount; i++)
+        {
+            float resolution = 0;
+            var shapeStart = reader.BaseStream.Position;
+            bool wasMassArray = _massArray.Count > 0;
+            
+        }
 
     }
 }
